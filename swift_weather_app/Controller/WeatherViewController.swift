0a26beccf597ce6b3fd71b4bfe09821be9862ac2
SDushan <samithadushan@gmail.com>
2021-10-24 13:31:01 +0530

    handle search field

--- swift_weather_app/Controller/WeatherViewController.swift
+++ swift_weather_app/Controller/WeatherViewController.swift
@@ -7,16 +7,37 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
-
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+    
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextFeild: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextFeild.delegate = self
     }
-
-
+    
+    @IBAction func searchPressed(_ sender: Any) {
+        searchTextFeild.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextFeild.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type Something"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        searchTextFeild.text = ""
+    }
 }
