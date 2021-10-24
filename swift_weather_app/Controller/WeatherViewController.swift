d7c0f5c178947c7c6718b5b9d09d70963d10bafc
SDushan <samithadushan@gmail.com>
2021-10-24 11:32:22 +0530

    added Weather VC

---
+++ swift_weather_app/Controller/WeatherViewController.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherViewController.swift
+//  swift_weather_app
+//
+//  Created by Dushan on 2021-10-24.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
