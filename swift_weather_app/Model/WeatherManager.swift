368013790282d153ca12be261bee04a1ece5c78f
SDushan <samithadushan@gmail.com>
2021-10-25 03:13:54 +0530

    added API key

--- swift_weather_app/Model/WeatherManager.swift
+++ swift_weather_app/Model/WeatherManager.swift
@@ -9,7 +9,7 @@ import Foundation
 
 
 struct WeatherManager {
-    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid={API Key}&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=757823edcf1c08d6d81a68c72e21c655&units=metric"
     
     func fetchWeather(cityName: String){
         let urlString = "\(weatherURL)&q=\(cityName)"
