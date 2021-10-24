650455f642fea7a64d39f25783dd9903f10ede68
SDushan <samithadushan@gmail.com>
2021-10-24 10:29:27 +0530

    init project

---
+++ swift_weather_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift_weather_app
+//
+//  Created by Dushan on 2021-10-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
