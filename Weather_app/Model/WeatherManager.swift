1ddf334ed813a62b3fc4206d1f0844667d207916
SDushan <samithadushan@gmail.com>
2021-10-25 11:41:05 +0530

    bind weather model data in weather view controller

--- Weather_app/Model/WeatherManager.swift
+++ Weather_app/Model/WeatherManager.swift
@@ -9,7 +9,7 @@ import Foundation
 
 
 struct WeatherManager {
-    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=757823edcf1c08d6d81a68c72e21c655&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid={API_Key}&units=metric"
     
     func fetchWeather(cityName: String){
         let urlString = "\(weatherURL)&q=\(cityName)"
@@ -37,7 +37,13 @@ struct WeatherManager {
         let decoder = JSONDecoder()
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-            print(decodedData.name)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            print(weather.conditionName)
+            print(weather.temperatureString)
         } catch {
             print(error)
         }
